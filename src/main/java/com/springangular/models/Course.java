d5ff77945093d108bc9f6bc87a8ccd891feee3a7
mounirrouissi <mounirrouissi2@gmail.com>
2020-06-10 07:39:52 +0100

    second commit

--- src/main/java/com/springangular/models/Course.java
+++ src/main/java/com/springangular/models/Course.java
@@ -1,5 +1,12 @@
 package com.springangular.models;
 
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
 public class Course {
-    
+    @Id
+    private Long courseId;
+    @ManyToOne
+    private Student student;
+
 }
